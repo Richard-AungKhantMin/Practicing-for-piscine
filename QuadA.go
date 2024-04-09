c436ebb1c3624fed41670d32728403469647a65f
Richard-AungKhantMin <aungkhantmin.dev@gmail.com>
2024-04-09 10:13:23 -0700

    a

--- QuadA.go
+++ QuadA.go
@@ -5,26 +5,43 @@ import (
 )
 
 func main() {
-	QuadA(5, 3)
-	QuadA(1, 0)
-	QuadA(8, 0)
-	QuadA(1, 1)
-	QuadA(-1, 9)
-	QuadA(5, -2)
+
+	/*
+		QuadA(5, 3)
+		QuadA(1, 5)
+
+		QuadA(1, 0)
+		QuadA(0, 1)
+		QuadA(8, 0)
+		QuadA(0, 13)
+
+		QuadA(1, 1)
+
+		QuadA(-1, 9)
+
+		QuadA(9, 1)
+
+		QuadA(5, -2)
+	*/
+
+	QuadA(10, 10)
 }
 
-func QuadA(x, y int) string {
-	if x < 0 || y < 0 {
-		fmt.Println("Invalid input, bro. Are you crazy?")
+func QuadA(x, y int) {
+	//invalid
+	if x <= 0 || y <= 0 {
+		fmt.Println()
+		return
 	}
 
+	// for the case, (1, not 1)
 	if x == 1 && y > 1 {
-		for i := 1; i <= y; i++ {
+		for i := 1; i <= y; i++ { //creating rooms according to the input
 			if i == 1 {
-				fmt.Println("0")
+				fmt.Println("o")
 			}
 
-			if 2 <= x && i < y {
+			if 2 <= y && i < y { //I used i < y instead of i= < y-1. It's the same.
 				fmt.Println("|")
 			}
 
@@ -32,18 +49,86 @@ func QuadA(x, y int) string {
 				fmt.Println("o")
 			}
 		}
+
+		return
 	}
 
-	if x > 1 && y == 0 {
+	// for the case (not 1, 1)
+	if x > 1 && y == 1 {
+		for i := 1; i <= x; i++ {
 
+			if i == 1 {
+				fmt.Printf("%s", "o")
+			}
+
+			if 2 <= i && i < x {
+				fmt.Printf("%s", "-")
+			}
+
+			if i == x {
+				fmt.Printf("%s", "o")
+			}
+		}
+		fmt.Println()
+		return
 	}
 
-	if x == 0 && y == 0 {
+	// (1,1) case
+	if x == 1 && y == 1 {
 		fmt.Println("o")
+		return
 	}
 
+	// a very typical square
 	if x > 0 && y > 0 {
 
+		//1st line
+		for i := 1; i <= x; i++ {
+
+			if i == 1 {
+				fmt.Printf("%s", "o")
+			}
+
+			if 2 <= i && i < x {
+				fmt.Printf("%s", "-")
+			}
+
+			if i == x {
+				fmt.Printf("%s", "o")
+			}
+		}
+		fmt.Println() // printing next line after 1st horizontal line
+
+		//the middle lines
+		for i := 1; i < y-1; i++ { // rows no. 2 to the one before the last
+			fmt.Printf("%s", "|") // first | string
+
+			for j := 2; j <= x-1; j++ {
+				fmt.Printf("%s", " ")
+			}
+
+			fmt.Printf("%s", "|") // last | string
+			fmt.Println()
+		}
+
+		// the last line
+		for i := 1; i <= x; i++ {
+
+			if i == 1 {
+				fmt.Printf("%s", "o")
+			}
+
+			if 2 <= i && i < x {
+				fmt.Printf("%s", "-")
+			}
+
+			if i == x {
+				fmt.Printf("%s", "o")
+			}
+
+		}
+		fmt.Println()
+		return
 	}
 
 }
