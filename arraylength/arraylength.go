08f8f8c65ed5fdb7a38080bcf1673ec881b9b1ef
amin <aungkhantmin.dev@gmail.com>
2024-04-13 21:56:40 +0300

    ok

---
+++ arraylength/arraylength.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func arraylength() {
+	arr := []int{10, 20, 30, 40, 50}
+	var length int
+
+	for i := range arr {
+		length = i + 1
+	}
+
+	fmt.Println("Length of the array is:", length)
+}
