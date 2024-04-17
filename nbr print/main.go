7c5a97d768e229409a67eacb203de2d87333d332
amin <aungkhantmin.dev@gmail.com>
2024-04-17 20:33:54 +0300

    a

--- nbr print/main.go
+++ nbr print/main.go
@@ -1,50 +1,23 @@
-package main
+package piscine
 
 import (
 	"github.com/01-edu/z01"
 )
 
-func main() {
-	PrintNbr(-123)
-	PrintNbr(0)
-	PrintNbr(123)
-	z01.PrintRune('\n')
-}
-
 func PrintNbr(n int) {
-	var sign rune
-	var newV int
-	var collector []rune
-
-	if n == 0 {
-		z01.PrintRune('0')
-		return
-	}
-
+	t := 1
 	if n < 0 {
-		sign = '-'
-		newV = n * -1
+		t = -1
+		z01.PrintRune('-')
+	}
+	if n != 0 {
+		f := (n / 10) * t
+		if f != 0 {
+			PrintNbr(f)
+		}
+		k := (n % 10 * t) + '0'
+		z01.PrintRune(rune(k))
 	} else {
-		newV = n
-	}
-
-	for newV > 0 {
-		collector = append(collector, rune((newV%10)+'0'))
-		newV = newV / 10
-	}
-
-	if n < 0 {
-		z01.PrintRune(sign)
-		for i := len(collector) - 1; i >= 0; i-- {
-			z01.PrintRune(collector[i])
-		}
-		return
-	}
-
-	if n > 0 {
-		for i := len(collector) - 1; i >= 0; i-- {
-			z01.PrintRune(collector[i])
-		}
-		return
+		z01.PrintRune('0')
 	}
 }
