c3cac704ec1810d812e34ec324dc9feb497eb9e1
amin <aungkhantmin.dev@gmail.com>
2024-09-13 22:36:27 +0300

    changes

--- package1/main.go
+++ package1/main.go
@@ -15,6 +15,9 @@ func main() {
 
 	NintMayLin := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 
+	NintMayLin[2] = 1978
+	NintMayLin[3] = 556
+
 	fmt.Println(p1.Adder(num1, num2))
 	fmt.Println(p2.HalfDaSlice(NintMayLin))
 
