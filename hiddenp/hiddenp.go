c28bb0e5716903d6ff60213238b64b2a30814d6a
Richard-AungKhantMin <aungkhantmin.dev@gmail.com>
2024-09-29 20:07:25 +0300

    a

--- hiddenp/hiddenp.go
+++ hiddenp/hiddenp.go
@@ -7,11 +7,11 @@ import (
 
 func main() {
 
+	fmt.Printf("Arguments: %v\n", os.Args)
+
 	if len(os.Args) != 3 {
 		return
 	}
-
-	fmt.Printf("Arguments: %v\n", os.Args)
 
 	s1 := os.Args[1]
 	s2 := os.Args[2]
